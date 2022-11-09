6ee9247172f059fc314fafa2e1d5a9b43f4bd7f9
alramar <alramar069@gmail.com>
2022-11-09 11:37:41 +0100

    Unity III

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
     // Start is called before the first frame update
     void Start()
     {
-
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -16,9 +17,10 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + 30.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 30.0f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
     }
 }
